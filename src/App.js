33dcacc9235ce3569437882b3bee565ce1120c56
joacoabraldes <abraldesjoaquin@gmail.com>
2023-04-22 16:25:29 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Card from './components/Card';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <>
       <div>hola</div>
       </>
+      <Card />
     </div>
   );
 }
